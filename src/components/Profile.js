59c08b61f1e347e8d1aa2133916d9496e020a75e
kalaiyarasan876 <160699419+kalaiyarasan876@users.noreply.github.com>
2024-06-24 12:27:22 +0530

    third commint

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import authService from '../services/authService';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import authService from "../services/authService";
 
 const Profile = () => {
-    const currentUser = authService.getCurrentUser();
-    const navigate = useNavigate();
+  // const currentUser = authService.getCurrentUser();
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        authService.logout();
-        navigate('/');
-    };
+  const handleLogout = () => {
+    authService.logout();
+    navigate("/");
+  };
 
-    if (!currentUser) {
-        return <h2>You are not logged in</h2>;
-    }
+  // if (!currentUser) {
+  //     return <h2>You are not logged in</h2>;
+  // }
 
-    return (
-        <div>
-            <h2>Profile</h2>
-            <p><strong>Username:</strong> {currentUser.user.username}</p>
-            <p><strong>Email:</strong> {currentUser.user.email}</p>
-            <button onClick={handleLogout}>Logout</button>
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">
+              Kalai
+            </a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item"></li>
+            </ul>
+
+            <button className="btn btn-danger " onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
         </div>
-    );
+      </nav>
+
+      {/* <h2>Profile</h2>
+            <p><strong>Username:</strong> {currentUser.user.username}</p>
+            <p><strong>Email:</strong> {currentUser.user.email}</p> */}
+    </div>
+  );
 };
 
 export default Profile;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,50 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import authService from '../services/authService';
-
+import authService from "../services/authService";
 
 const Register = () => {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
-  
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        try {
-            await authService.register(username, email, password);
-            setMessage('User registered successfully!');
-            window.alert("Registeration Successfully");
-            navigate("/");
-         
-           
-        } catch (error) {
-            setMessage('Error: ' + error.response.data.msg);
-        }
-    };
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-    return (
-        <div>
-            <form onSubmit={handleRegister}>
-                <h2>Register</h2>
-                <div>
-                    <label>Username</label>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                <button type="submit" >Register</button>
-            </form>
-            {message && <p>{message}</p>}
-        </div>
-    );
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await authService.register(username, email, password);
+      setMessage("User registered successfully!");
+      window.alert("Registeration Successfully");
+      navigate("/");
+    } catch (error) {
+      setMessage("Error: " + error.response.data.msg);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div class="d-flex justify-content-center h-100 mt-5 ">
+        <form onSubmit={handleRegister}>
+          <h2>Register</h2>
+          <div>
+            <label>Username</label>
+            <input
+              type="text"
+              value={username}
+               class="form-control" placeholder="username"
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              class="form-control" placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              class="form-control" placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button  className="btn btn-success my-2 mt-3" type="submit">Register</button>
+        </form>
+        {message && <p>{message}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Register;
